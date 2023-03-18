e7cc70e695c1294c2433c80b3b9a476fca32ef23
Jefferson Gibin <jefferson@jefferson.gibin>
2023-03-18 18:47:26 -0300

    updated index.js

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ import { RoutesHandler } from "./src/handler/routes-handler.js";
 import { signInRoute } from "./src/app/routes/sign-in.route.js";
 
 const app = express();
-const authRouter = express.Router();
 
 // Parse to Json
-authRouter.use(express.json());
-
-// Define the Context of api
-app.use("/auth", authRouter);
+app.use(express.json());
 
 // router list
 const routes = [signInRoute];
 
 // apply routes
-new RoutesHandler(authRouter, routes).apply();
+new RoutesHandler(app, routes).apply();
 
 export const handler = serverless(app);
