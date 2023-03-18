8ebf71751e3e1dc407f56e4bb7aa98b957867e8e
Jefferson Gibin <jefferson@jefferson.gibin>
2023-03-18 16:23:30 -0300

    DPP-22 implemented router sign-in

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import serverless from "serverless-http";
 import express from "express";
 import { RoutesHandler } from "./src/handler/routes-handler.js";
+import { signInRoute } from "./src/app/routes/sign-in.route.js";
 
 const app = express();
 app.use(express.json());
 
 // router list
-const routes = [];
+const routes = [signInRoute];
 
 // apply routes
 new RoutesHandler(app, routes).apply();
