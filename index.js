60a1b82dfeebfcbed1d1df48dd562ef2820f08f3
Jefferson Gibin <jefferson@jefferson.gibin>
2023-03-21 13:45:11 -0300

    apply cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import serverless from "serverless-http";
 import express from "express";
+import cors from "cors";
 import { RoutesHandler } from "./src/handler/routes-handler.js";
 import { signInRoute } from "./src/app/routes/sign-in.route.js";
 
 const app = express();
+
+// Apply cors
+app.use(cors());
+
 const authRouter = express.Router();
 
 // Parse to Json
